19c0295e512f3a2f84a26e77af238ab5cd92a37b
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-06-06 22:49:43 +0100

    add variable names to each select tag in main.js

--- src/main.js
+++ src/main.js
@@ -4,3 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 
 import  './assets/images/currency.png';
+
+let api = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD`
+const fromDropDown = document.querySelector('#from-currency-select');
+const toDropDown = document.querySelector('#to-currency-select');
